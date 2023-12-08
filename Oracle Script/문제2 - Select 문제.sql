764c7f51ae6473afcd063f181634fdd6393a483b
soomin Kang <tnals3256@naver.com>
2023-12-08 13:19:28 +0900

    12월 08일 점심

--- Oracle Script/문제2 - Select 문제.sql
+++ Oracle Script/문제2 - Select 문제.sql
@@ -23,31 +23,53 @@ select*from employee;
 select ename as "사원이름", salary as "급여" , salary +300 as "인상된급여"
 from employee;
 
+desc employee; 
+
+select * 
+from employee; 
+
+select eno as 사원번호, ename 사원명, job 직책, manager "직속상관  ID",
+    hiredate 입사날짜, salary 월급, commission 보너스, dno 부서번호
+from employee; 
+
+select ename, salary, salary + 300 as 인상된급여
+from employee; 
 --2. 사원의 이름, 급여, 연간 총 수입이 많은것 부터 작은순으로 출력 하시오. 
 --    연간 총 수입은 월급에 12를 곱한후 $100의 상여금을 더해서 계산 하시오. 
 select ename as "사원이름", salary as "급여" , (salary * 12) + 100 AS "연간총수입"
 from employee;
 
+-- 연결된 DataBase에서 모든 테이블 출력 
+select*from tab;  -- tab=table
+
+select ename, salary, commission from employee; 
+    
+select ename, salary,commission,salary *12, salary *12 + NVL(commission,0) + 100 as 총연봉
+from employee;   
 --3. 급여가 2000을 넘는 사원의 이름과 급여를 급여가 많은것 부터 작은순으로 출력하세요. 
-select ename as "사원이름",salary as "급여"
-from employee
-where salary>2000
-order by salary DESC;
+desc employee;
+
+select ename as "사원이름",salary as "급여"   -- 컬럼명
+from employee                               -- 테이블, 뷰
+where salary>2000                           -- 조건   
+order by salary DESC                        -- 정렬
 
 --4. 사원번호가 7788인 사원의 이름과 부서번호를 출력하세요.
 select ename as "사원이름",dno as "부서번호"
 from employee
-where eno = 7788;
+where eno = 7788;  --ebo no타입
 
 --5. 급여가 2000에서 3000사이에 포함되지 않는 사원의 이름과 급여를 출력 하세요. 
 select ename as "사원이름", salary as "급여"
 from employee
+where salary not between 2000 and 3000;
+
 where salary < 2000 or salary > 3000 ;
 
 --6. 1981년 2월 20일부터 81년 5월 1일 사이의 입사한 사원의 이름 담당업무, 입사일을 출력하시오
 select ename as "사원이름", job as "담당업무",  hiredate as "입사일"
 from employee
-WHERE hiredate BETWEEN '81-02-20' AND '81-05-01';
+WHERE hiredate BETWEEN '1981-02-20' AND '1981-05-01';  --between A and B '' '' 넣어야됨
 
 --7. 부서번호가 20및 30에 속한 사원의 이름과 부서번호를 출력하되 이름을 기준(내림차순)으로 출력하시오.
 select ename as "사원이름",dno as "부서번호"
@@ -55,24 +77,37 @@ from employee
 where dno in(20,30)
 order by ename desc;
 
+select ename, dno 
+from employee
+where dno = 20 or dno = 30 
+order by ename asc; 
+
 --8. 사원의 급여가 2000에서 3000사이에 포함되고 부서번호가 20 또는 30인 사원의 이름, 급여와 부서번호를 출력하되 이름을 오름차순으로 출력하세요.
 select ename as "사원이름",salary as "급여", dno as "부서번호"
 from employee
 where salary between 2000 and 3000
-   and (dno= 20 or dno= 30) 
-order by ename asc; 
+and (dno= 20 or dno= 30) 
 
 --9. 1981년도 입사한 사원의 이름과 입사일을 출력 하시오 ( like 연산자와 와일드 카드 사용 : _ , % )
 select ename as "사원이름", hiredate as "입사일"
 from employee 
-where hiredate like '1981%';
+where hiredate like '81%';
+
+select ename as "사원이름", hiredate as "입사일"
+from employee 
+where substr(hiredate,1,2)-'81';
 
 --10. 관리자가 없는 사원의 이름과 담당업무를 출력하세요.
+select * from employee; 
+
 select ename as "사원이름",job as "담당업무"
 from employee
 where manager IS NULL;
 
 --11. 커밋션을 받을 수 있는 자격이 되는 사원의 이름, 급여, 커미션을 출력하되 급여 및 커밋션을 기준으로 내림차순 정렬하여 표시하시오.
+select * from employee
+order by commission desc
+
 select ename as "사원이름",salary as "급여",commission as "커미션"
 FROM employee
 WHERE commission IS NOT NULL
@@ -81,13 +116,24 @@ ORDER BY salary DESC, commission DESC;
 --12. 이름이 세번째 문자인 R인 사원의 이름을 표시하시오.
 select ename as "사원이름"
 from employee
-where SUBSTR(ename from 3 for 1 )= 'R';
+where ename like '__R%' --__
+
+select ename
+from employee
+where instr(ename, 'R', 3)=3;
+
+select ename , instr(ename, 'R', 3) from employee; 
 
 --13. 이름에 A 와 E 를 모두 포함하고 있는 사원의 이름을 표시하시오.
 select ename as "사원이름"
 from employee
 where ename like '%A%' and ename like '%E%'; 
 
+select ename 
+from employee
+where instr(ename, 'A')!=0 and instr(ename, 'E')!=0;
+
+select ename , instr (ename, 'A') , instr(ename , 'E') from employee; 
 --14. 담당 업무가 사무원(CLERK) 또는 영업사원(SALESMAN)이며서 
 --    급여가 $1600, $950, 또는 $1300 이 아닌 사원의 이름, 담당업무, 급여를 출력하시오.
 select ename as "사원이름",job as "담당업무",salary as "급여"   
--- Oracle Script/문제3- Select 문제.sql
+++ Oracle Script/문제3- Select 문제.sql
@@ -29,6 +29,9 @@ substr(hiredate, 1, 2) as 입사년도,
 substr(hiredate, 4, 2) as 입사달
 from employee;
 
+select * from employee; 
+select substr ( hiredate, 1,5 ) as 년월 
+from employee; 
 --2. SUBSTR 함수를 사용하여 4월에 입사한 사원을 출력 하시오.
 select ename as 사원, hiredate 입사일, substr(hiredate, 4,2)
 from employee
@@ -39,21 +42,51 @@ SELECT *
 FROM employee
 WHERE MOD(MANAGER, 2) = 1;
 
-4. MOD 함수를 사용하여 월급이 3의 배수인 사원들만 출력하세요.
-5. 입사한 년도는 2자리 (YY), 월은 (MON)로 표시하고 요일은 약어 (DY)로 지정하여 출력 하시오. 
-6. 올해 몇 일이 지났는지 출력 하시오. 현재 날짜에서 올해 1월 1일을 뺀 결과를 출력하고 TO_DATE 함수를 사용하여
-   데이터 형식을 일치 시키시오.
-7. 자신이 태어난 날짜에서 현재까지 몇 일이 지났는지 출력 하세요. 
-8. 자신이 태어난 날짜에서 현재까지 몇 개월이 지났는지 출력 하세요.
-9. 사원들의 상관 사번을 출력하되 상관이 없는 사원에 대해서는 null 갑대신 0으로 출력 하시오.
-10.   사원번호,
+select ename, manager 
+from employee
+where MOD (manager , 2) = 1; 
+
+--4. MOD 함수를 사용하여 월급이 3의 배수인 사원들만 출력하세요.
+select ename, salary 
+from employee
+where MOD ( salary , 3) = 0 ; 
+
+--5. 입사한 년도는 2자리 (YY), 월은 (MON)로 표시하고 요일은 약어 (DY)로 지정하여 출력 하시오. 
+select hiredate , to_char ( hiredate, 'YY MON DD DY') as 출력결과 
+from employee; 
+
+--6. 올해 몇 일이 지났는지 출력 하시오. 현재 날짜에서 올해 1월 1일을 뺀 결과를 출력하고 TO_DATE 함수를 사용하여
+--   데이터 형식을 일치 시키시오.
+select trunc (sysdate - to_date ( 20220101 , 'YYYYMMDD')) as 올해날짜수 from dual  
+
+--7. 자신이 태어난 날짜에서 현재까지 몇 일이 지났는지 출력 하세요. 
+ select trunc (sysdate - to_date ( 19800101 , 'YYYYMMDD')) as 총날짜수 from dual  
+
+--8. 자신이 태어난 날짜에서 현재까지 몇 개월이 지났는지 출력 하세요.
+select trunc( months_between (sysdate , to_date ( 19800101 , 'YYYYMMDD'))) as 총개월수 from dual 
+
+--9. 사원들의 상관 사번을 출력하되 상관이 없는 사원에 대해서는 null 갑대신 0으로 출력 하시오.
+select ename, manager , nvl (manager, 0), nvl2 (manager, manager,0)
+from employee; 
+
+/*10.   사원번호,
       [사원번호 2자리만출력 나머지는 *가림 ] as "가린번호", 
       이름, 
-       [이름의 첫자만 출력 총 네자리, 세자리는 * 가림] as "가린이름"       
+      [이름의 첫자만 출력 총 네자리, 세자리는 * 가림] as "가린이름"     
+*/ 
+select eno, rpad (substr (eno , 1 ,2) , length(eno), '*')  as 가린번호 ,  
+ename, rpad ( substr (ename, 1,1), 4,'*')
+from employee;
+
+select eno, rpad (substr (eno , 1 ,2) , length(eno), '*')  as 가린번호 ,  
+ename, rpad ( substr (ename, 1,1), length (ename),'*')
+from employee;
+
+select ename, length(ename)
+from employee; 
+
 11.  주민번호:   를 출력하되 801210-1*******   출력 하도록 , 전화 번호 : 010-12*******
 	dual 테이블 사용
-    
-12 자신의 생일에서 현재까지 몇개월 살았는지 ?  몇일 살았는지 출력   <== months_between ( 현재날짜, 생일)  : 개월수 
-        -- sysdate - 생일 (date) 
-        
-13.  employee 테이블에서 2050년 12월 24 일까지의 날짜(일수)  를 출력 
+select  RPAD (substr ('801210-1234578', 1,8) , length ('801210-1234578'), '*') as 주민번호,
+        RPAD ( substr ('010-1234-5678', 1,6), length ('010-1234-5678'), '*') as 전화번호
+        from dual ;     
