5098c04a61d5705c2b3e2d6a01a28f33af4adec1
soomin Kang <tnals3256@naver.com>
2023-12-13 13:57:39 +0900

    12월 13일 13시

--- Oracle Script/HR- DQL,DML.sql
+++ Oracle Script/HR- DQL,DML.sql
@@ -64,6 +64,8 @@ desc account10;
 
 /*
    DML : insert(값을 입력) , update(수정), delete(삭제)
+     <== 트랜잭션 자동 시작, 
+     <== 트랜잭션 종료 ( rollback, commit)는 명시 (오라클)
 */
 
 -- 테이블 복사
@@ -97,3 +99,111 @@ values(60,'인사부','부산');
 
 commit;
 
+--칼럼 생략 할때는 모든 칼럼에 값이 순서에 맞게 입력 
+insert into dept03 
+values(60,'인사부'); -- 오류코드 부산까지 넣어줘야됨
+
+-- 칼럼을 명시할때 순서를 바꿀 수 있고, 특정칼럼은 값을 넣지 않아도 됨.
+insert into dept03(dname, dno)
+values('영업부',80);
+
+select*from dept03;
+commit;
+
+
+-- UPDATE 문 : 입력된 값을 수정시 사용, 반드시 where 조건을 사용, where 조건에 사용되는 칼럼은 Primary Key 칼럼이어야 함.
+/*
+ update 테이블명
+ set 칼럼명 = 바꿀값, 칼럼명=바꿀값
+ where 조건 
+*/
+
+--중요 
+update dept03 
+set loc = '대구'
+where dno = 80;
+
+update dept03
+set dname = '관리부', loc = '광주'
+where dno = 40;
+
+rollback;
+
+select*from dept03;
+
+--
+insert into dept03
+values(90,'영업부','대구');
+
+insert into dept03
+values(91,'영업부','광주');
+
+commit;
+-- update에서 반드시 where 조건, 조건을 처리하는 칼럼은 Primary key, Unique 키 칼럼을 정의
+update dept03
+set dname = '인쇄부'
+where loc = '광주';  -- 중복된 값
+
+-- update 시 primary key 칼럼을 조건으로 처리해서 원하는 값만 수정
+update dept03
+set dname = '인쇄부'
+where dno in(91,80);
+
+-- Delete : 레코드를 삭제 할때, Where<조건>, <조건: 중복되지 않는 칼럼>
+/*
+  delete 테이블명
+  where 조건
+*/
+
+select*from dept03;
+
+--delete문에서 조건을 사용하지 않는 경우 모든 레코드가 삭제됨
+delete dept03
+rollback;
+
+-- delete, where 조건 사용,  <== 
+delete dept03
+where dno = 91;
+
+-- 모든 레코드를 삭제 :
+   /* delete                : 레코드 하나하나를 삭제함, 시간이 오래 걸림. << 로우레벨 포멧>>       
+      truncate table 테이블명 :    한꺼번에 통째로 모든 레코드를 날림. <<빠른 포멧>>
+      
+      drop table 테이블명    :  테이블 자체를 삭제
+    */
+select*from dept03;    
+rollback;
+commit;
+
+-- 모든 레코드 삭제 : 값만 삭제 롤백가능
+delete dept03;
+
+-- 모든 레코드 삭제 : 값만 삭제  <== DDL 문이기 때문에 Rollback이 안됨. 실행후 바로 커밋됨
+  -- 빠르게 처리됨
+  
+truncate table dept03;
+-------------------------------------------------------------
+create table emp04
+as
+select*from employee;
+
+select*from emp04;
+
+/*
+   임의의 값을 추가: insert
+   임의의 값을 수정: update
+   임의의 값을 삭제: delete
+*/
+desc emp04;
+select*from user_constraints where table_name in('EMP04');
+
+insert into emp04(eno,ename,job,dno)
+values(7899,'KANG','WORKER',20);
+
+update emp04
+set manager = 7777
+where eno = 7899;
+
+delete emp04;
+rollback;
+commit;
